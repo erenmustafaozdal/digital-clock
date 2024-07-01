d052684c816b684ef8ffa192c684b8b9da8d99eb
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-07-01 16:05:50 +0300

    LCD santigrad derece sembolü ve diğer işlemler

--- lib/Display/Display.cpp
+++ lib/Display/Display.cpp
@@ -1,17 +1,41 @@
 #include "Display.h"
+#include <Arduino.h>
 
-Display::Display(int rs, int en, int d4, int d5, int d6, int d7)
-    : lcd(rs, en, d4, d5, d6, d7) {}
+// Derece sembolü için özel karakter dizisi
+byte degreeSymbol[8] = {
+    B00111,
+    B00101,
+    B00111,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000};
+
+Display::Display(int rs, int en, int d4, int d5, int d6, int d7, int backlightPin)
+    : lcd(rs, en, d4, d5, d6, d7), _backlightPin(backlightPin) {}
 
 void Display::initialize()
 {
+    pinMode(_backlightPin, OUTPUT);
     lcd.begin(16, 2);
+    lcd.createChar(0, degreeSymbol); // Derece sembolünü özel karakter olarak tanımla
 }
 
 void Display::print(int col, int row, const char *message)
 {
     lcd.setCursor(col, row);
     lcd.print(message);
+}
+
+void Display::temp(int col, int row, const char *message)
+{
+    lcd.setCursor(col, row);
+    lcd.print(message);
+    lcd.setCursor(col + strlen(message), row);
+    lcd.write(byte(0)); // Derece simgesini yazdırma
+    lcd.setCursor(col + strlen(message) + 1, row);
+    lcd.print("C");
 }
 
 void Display::clear()
--- lib/Display/Display.h
+++ lib/Display/Display.h
@@ -6,13 +6,15 @@
 class Display
 {
 public:
-    Display(int rs, int en, int d4, int d5, int d6, int d7);
+    Display(int rs, int en, int d4, int d5, int d6, int d7, int backlightPin);
     void initialize();
     void print(int col, int row, const char *message);
     void clear();
+    void temp(int col, int row, const char *message);
 
 private:
     LiquidCrystal lcd;
+    int _backlightPin;
 };
 
 #endif
